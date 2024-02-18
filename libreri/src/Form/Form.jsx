82225b25dbc04eb203804ee252fd666594f2fb9f
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-18 10:52:58 +0100

    Create Form.jsx

---
+++ libreri/src/Form/Form.jsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
+};
+export default Form;
