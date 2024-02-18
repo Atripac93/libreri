29099933284f06bf611894040e3a2ee5a1c01750
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-18 10:53:04 +0100

    Create HomePage.jsx

---
+++ libreri/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Footer/Footer";
+import EnterPAge from "../Main/EnterPage";
+import Navigation from "../Navbar/Navigation";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navigation />
+      <EnterPAge />
+      <Footer />
+    </>
+  );
+};
+export default HomePage;
