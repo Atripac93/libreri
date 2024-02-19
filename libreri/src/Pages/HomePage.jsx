eed58ad945e99436660a9870405d2525b90c277b
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-19 21:36:21 +0100

    Update HomePage.jsx

--- libreri/src/Pages/HomePage.jsx
+++ libreri/src/Pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import Footer from "../Footer/Footer";
 import EnterPAge from "../Main/EnterPage";
 import Navigation from "../Navbar/Navigation";
-import AddComment from "../Form/AddComment";
 
 const HomePage = () => {
   return (
     <>
       <Navigation />
-      <AddComment />
       <EnterPAge />
       <Footer />
     </>
